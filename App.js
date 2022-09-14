a6b00e03144751f827848f585b9a0c10260d2eb2
Lateef Ahmed <lateefahmed621@gmail.com>
2022-09-14 14:03:10 +0100

    styling app

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StackNavigation } from "./infrastructure/navigation/StackNav";
-import { Service } from "./infrastructure/screens/Service"
-import { Home } from "./infrastructure/screens/Home";
 
 
 export default function App() {
     return(
-        // <NavigationContainer>
-        //     <StackNavigation/>
-        // </NavigationContainer>
-            
-        // <Service/>
-        <Home/>
+        <NavigationContainer>
+            <StackNavigation/>
+        </NavigationContainer>
     )
 }
